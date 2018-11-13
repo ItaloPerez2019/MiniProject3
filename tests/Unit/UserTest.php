14380e365f58b21faf8ddfc06ff96b7de57d2203
italoperez2019 <Itl00147441!>
2018-11-13 13:22:14 -0500

    Unit Test User

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -22,5 +23,23 @@ class UserTest extends TestCase
         $response = $this->get('/register');
 
         $response->assertStatus(200);
+    }
+    public function testInsertUser()
+    {
+        $data =  [
+            'name' => str_random(5),
+            'email' => str_random(6).'@testing.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ];
+
+        $user = User::create($data);
+
+        $this->assertInstanceOf(User::class, $user);
+
+        $this->assertEquals($data['name'], $user->name);
+        $this->assertEquals($data['email'], $user->email);
+        $this->assertEquals($data['password'], $user->password);
+
     }
 }
