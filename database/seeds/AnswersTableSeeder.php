6f080fa8c66530ff52b64ff1886f03af9b9e921b
italoperez2019 <Itl00147441!>
2018-11-12 21:01:05 -0500

    Answer Branch

---
+++ database/seeds/AnswersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AnswersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+
+    {
+        factory(App\Answer::class, 50)->create()->each(function ($answers) {
+//            $user->posts()->save(factory(App\Post::class)->make());
+        });
+    }
+}
